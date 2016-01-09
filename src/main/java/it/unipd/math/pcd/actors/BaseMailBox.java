40e805bafb00cb7cb991ff906be0c523154ecc4f
giacomocusinato <giacomo.cusinato@outlook.com>
2016-01-09 17:07:11 +0100

    Add mailbox components

---
+++ src/main/java/it/unipd/math/pcd/actors/BaseMailBox.java
@@ -0,0 +1,68 @@
+/**
+ * The MIT License (MIT)
+ * <p/>
+ * Copyright (c) 2015 Giacomo Cusinato
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * <p/>
+ * Please, insert description here.
+ *
+ * @author Giacomo Cusinato
+ * @version 1.0
+ * @since 1.0
+ */
+
+/**
+ * Please, insert description here.
+ *
+ * @author Giacomo Cusinato
+ * @version 1.0
+ * @since 1.0
+ */
+package it.unipd.math.pcd.actors;
+
+import java.util.ArrayList;
+
+/**
+ * Defines a basic mailbox, a simple container that manages the incoming
+ * messages of a specific actor.
+ *
+ * @author Riccardo Cardin
+ * @version 1.0
+ * @since 1.0
+ */
+public class BaseMailBox<T extends Message> implements MailBox<T> {
+
+    protected ArrayList<T> list = new ArrayList<>();
+
+    @Override
+    public synchronized boolean add(int index, T message) {
+        this.list.add(index, message);
+    }
+
+    @Override
+    public synchronized boolean add(T message) {
+        this.list.add(message);
+    }
+
+    @Override
+    public synchronized boolean remove(T message) {
+        return this.list.remove(message);
+    }
+}
---
+++ src/main/java/it/unipd/math/pcd/actors/MailBox.java
@@ -0,0 +1,71 @@
+/**
+ * The MIT License (MIT)
+ * <p/>
+ * Copyright (c) 2015 Giacomo Cusinato
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * <p/>
+ * Please, insert description here.
+ *
+ * @author Giacomo Cusinato
+ * @version 1.0
+ * @since 1.0
+ */
+
+/**
+ * Please, insert description here.
+ *
+ * @author Giacomo Cusinato
+ * @version 1.0
+ * @since 1.0
+ */
+package it.unipd.math.pcd.actors;
+
+/**
+ * Defines the interface of the mailbox, a container that manages the incoming
+ * messages of a specific actor.
+ *
+ * @author Riccardo Cardin
+ * @version 1.0
+ * @since 1.0
+ */
+public interface MailBox<T extends Message> {
+
+    /**
+     * Adds a message at the end of the mailbox.
+     *
+     * @param message The type of messages the actor can receive
+     */
+    boolean add(T message);
+
+    /**
+     * Adds a message at a defined index in the mailbox.
+     *
+     * @param message The type of messages the actor can receive
+     * @param index The index of the list to add the message to.
+     */
+    boolean add(int index, T message);
+
+    /**
+     * Removes a message from the mailbox.
+     *
+     * @param message The type of messages the actor can receive
+     */
+    boolean remove(T message);
+}
